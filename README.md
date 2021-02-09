9c536cc8b2ae6cfb74ef30ebafe2f5e4fccb6bc4
Ricardo Schroeder <ricardoschroeder@gmail.com>
2021-02-09 21:48:06 +0100

    Fixed issues

--- README.md
+++ README.md
@@ -1,12 +1,17 @@
-My name is Ricardo and today is Tuesday, 9 February, 19:17 CET.
+<p>Welcome to my page! </br> I'm Ricardo, Full-Stack developer from <b>Santa Cruz do Sul, Brazil</b>, currently living in <b>Amsterdam, Netherlands</b>. </p>
 
+Currently, the weather is: <b> -3°C, <i>few clouds</i></b></br>Today, the sun rises at <b>08:07 AM</b> and sets at <b>05:41 PM</b>.</p>
+
+<p align="center">This <i>README</i> file is generated <b>every 3 hours</b>!</br>Last refresh: Tuesday, February 9, 9:47 PM GMT+1<br /><a href="https://medium.com/@th.guibert/how-to-create-a-self-updating-readme-md-for-your-github-profile-f8b05744ca91">Create your own here!</a></p>
+![README build](https://github.com/ricardoschroeder/ricardoschroeder/workflows/README%20build/badge.svg)
+<!--
 ![Ricardo's github stats](https://github-readme-stats.vercel.app/api?username=ricardoschroeder&show_icons=true&theme=dark)
 
 ![Top Langs](https://github-readme-stats.vercel.app/api/top-langs/?username=ricardoschroeder&theme=dark)
 
 [![trophy](https://github-profile-trophy.vercel.app/?username=ricardoschroeder&theme=onedark)](https://github.com/ryo-ma/github-profile-trophy)
 
-<!--
+
 **ricardoschroeder/ricardoschroeder** is a ✨ _special_ ✨ repository because its `README.md` (this file) appears on your GitHub profile.
 
 Here are some ideas to get you started:
--- index.js
+++ index.js
@@ -1,31 +1,10 @@
 const Mustache = require('mustache');
 const fs = require('fs');
 const MUSTACHE_MAIN_DIR = './main.mustache';
-const https = require('https');
-
-let temperature = '';
-let apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
-
-https.get('https://api.openweathermap.org/data/2.5/weather?q=Amsterdam,nl&units=metric&appid=' + apiKey,
-  (resp) => {
-    let data = '';
-
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    resp.on('end', () => {
-      temperature = JSON.parse(data).$.main.temp;
-    });
-  })
-  .on("error", (err) => {
-    console.log("Error: " + err.message);
-  })
-
+const fetch = require('node-fetch');
 
 let DATA = {
-  name: 'Ricardo',
-  date: new Date().toLocaleDateString('en-GB', {
+  refresh_date: new Date().toLocaleDateString('en-GB', {
     weekday: 'long',
     month: 'long',
     day: 'numeric',
@@ -34,15 +13,48 @@ let DATA = {
     timeZoneName: 'short',
     timeZone: 'Europe/Amsterdam',
   }),
-  temperature: temperature
 };
 
-function generateReadMe() {
-  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+async function setWeatherInformation() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=amsterdam,nl&appid=${process.env.OPEN_WEATHER_MAP_API_KEY}&units=metric`
+  )
+    .then(r => r.json())
+    .then(r => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Amsterdam',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-GB', {
+        hour: '2-digit',
+        minute: '2-digit',
+        timeZone: 'Europe/Amsterdam',
+      });
+    });
+}
+
+async function generateReadMe() {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
   });
 }
 
-generateReadMe();
+async function action() {
+  /**
+   * Fetch Weather
+   */
+  await setWeatherInformation();
+
+  /**
+   * Generate README
+   */
+  await generateReadMe();
+}
+
+action();
