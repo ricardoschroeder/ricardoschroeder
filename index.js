b9394d974ff74d7f4a6c9694ed73a6bd941d0b46
Ricardo Schroeder <ricardoschroeder@users.noreply.github.com>
2021-02-10 21:38:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
 const Mustache = require('mustache');
-const fs = require('fs');
-const MUSTACHE_MAIN_DIR = './main.mustache';
 const fetch = require('node-fetch');
+const fs = require('fs');
+
+const MUSTACHE_MAIN_DIR = './main.mustache';
 
 let DATA = {
   refresh_date: new Date().toLocaleDateString('en-GB', {
@@ -39,7 +40,7 @@ async function setWeatherInformation() {
 }
 
 async function generateReadMe() {
-  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) =>  {
+  await fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
     if (err) throw err;
     const output = Mustache.render(data.toString(), DATA);
     fs.writeFileSync('README.md', output);
